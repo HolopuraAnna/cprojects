e350148ae97b84ebbede7639e6e383bd64ab9212
HolopuraAnna <113437256+HolopuraAnna@users.noreply.github.com>
2022-09-13 14:51:56 +0300

    s

---
+++ 2_4.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+/*double func(double x){
+    double s1 = 1;
+    double s2 = s1 * x;
+    double s3 = s2 * x;
+    double s4 = s3 * x;
+    double s5 = s4 * x;
+    return s1 + s2 + s3 + s4 + s5;
+}*/
+
+int main()
+{
+    double x;
+    scanf("%lf", &x);
+    
+    double s = 4.7;
+    printf("S = %lf\n", s);
+}
