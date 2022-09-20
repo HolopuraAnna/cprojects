f6a7e69d8e84a4e71701fcbf8674cfdda6fc1927
HolopuraAnna <113437256+HolopuraAnna@users.noreply.github.com>
2022-09-20 12:32:04 +0300

    Delete Lab_1 directory

--- Lab_1/1_18.c
+++
@@ -1,9 +0,0 @@
-// 1_18
-
-#include <stdio.h>
-
-int main()
-{
-    char a[] = {'O','X',' ',' ','X','O','X','O',' '};
-    printf("%c | %c | %c\n%c | %c | %c\n%c | %c | %c\n", a[0], a[1], a[2], a[3], a[4], a[5], a[6], a[7], a[8]);
-}
