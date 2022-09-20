22675ff5cba29690d09486a897107d1258f6782d
HolopuraAnna <113437256+HolopuraAnna@users.noreply.github.com>
2022-09-20 12:37:43 +0300

    Holopura Anna, 1 lab

---
+++ Lab_1/1_3.c
@@ -0,0 +1,13 @@
+// 1_3
+
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    scanf("%i", &a);
+    printf("%i)\n", a);
+    printf("- %i - %i - %i\n", a, a, a);
+    printf("- %i - %i - %i\n", a, a, a);
+    printf("%i | %i | %i\n", a, a, a);
+}
